0d8c9463f7d11c3b4ae11ed4301bf756e419ebe3
Rahul Sinha <rahul.sinha1908@gmail.com>
2017-03-02 23:17:06 +0530

    IBM Watson API implemented

--- app/src/main/java/com/christhack/rsinha/christhack/MainActivity.java
+++ app/src/main/java/com/christhack/rsinha/christhack/MainActivity.java
@@ -1,13 +1,71 @@
 package com.christhack.rsinha.christhack;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
+import com.ibm.watson.developer_cloud.alchemy.v1.model.DocumentEmotion;
+import com.ibm.watson.developer_cloud.alchemy.v1.model.LanguageSelection;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText=(EditText)findViewById(R.id.sample_id);
+        button=(Button) findViewById(R.id.process_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new FetchData(editText.getText().toString()).execute();
+            }
+        });
+        //service.getEmotion();
+    }
+    class FetchData extends AsyncTask<Void, Void, Void>{
+        private String message;
+        public FetchData(String mess){
+            message=mess;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            AlchemyLanguage service = new AlchemyLanguage();
+            service.setApiKey("0fd9a51cd63514147b56e2a2cad1a0ccb2e97073");
+            service.setEndPoint("https://gateway.watsonplatform.net/natural-language-understanding/api");
+            service.setUsernameAndPassword("1655e4b1-ddae-411a-b9ec-77dbf4aa8f6c","6BqRQ0Jqhdnf");
+            service.setLanguage(LanguageSelection.ENGLISH);
+
+            Map<String, Object> myMap= new HashMap<String, Object>();
+            myMap.put("text", message);
+            Log.i(TAG, "Its Starting to process");
+            DocumentEmotion emotion = service.getEmotion(myMap).execute();
+            DocumentEmotion.Emotion emo = emotion.getEmotion();
+            Log.i(TAG, "Its got processed");
+
+            Log.i(TAG, "Anger : "+emo.getAnger());
+            Log.i(TAG, "Disgust : "+emo.getDisgust());
+            Log.i(TAG, "Fear : "+emo.getFear());
+            Log.i(TAG, "Joy : "+emo.getJoy());
+            Log.i(TAG, "Sadness : "+emo.getSadness());
+
+            return null;
+        }
     }
 }
