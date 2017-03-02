c6069348f3ff0f097801a75e857855d07c1bf199
Rahul Sinha <rahul.sinha1908@gmail.com>
2017-03-02 23:44:22 +0530

    Some bugs in retrieving the datas

--- app/src/main/java/com/christhack/rsinha/christhack/MainActivity.java
+++ app/src/main/java/com/christhack/rsinha/christhack/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.EditText;
 import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
 import com.ibm.watson.developer_cloud.alchemy.v1.model.DocumentEmotion;
 import com.ibm.watson.developer_cloud.alchemy.v1.model.LanguageSelection;
+import com.ibm.watson.developer_cloud.http.ServiceCall;
+import com.ibm.watson.developer_cloud.http.ServiceCallback;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -46,25 +48,52 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... params) {
 
-            AlchemyLanguage service = new AlchemyLanguage();
-            service.setApiKey("0fd9a51cd63514147b56e2a2cad1a0ccb2e97073");
-            service.setEndPoint("https://gateway.watsonplatform.net/natural-language-understanding/api");
-            service.setUsernameAndPassword("1655e4b1-ddae-411a-b9ec-77dbf4aa8f6c","6BqRQ0Jqhdnf");
-            service.setLanguage(LanguageSelection.ENGLISH);
+            try {
+                AlchemyLanguage service = new AlchemyLanguage();
+                service.setApiKey("0fd9a51cd63514147b56e2a2cad1a0ccb2e97073");
+                service.setEndPoint("https://gateway.watsonplatform.net/natural-language-understanding/api");
+                service.setUsernameAndPassword("1655e4b1-ddae-411a-b9ec-77dbf4aa8f6c", "6BqRQ0Jqhdnf");
+                service.setLanguage(LanguageSelection.ENGLISH);
 
-            Map<String, Object> myMap= new HashMap<String, Object>();
-            myMap.put("text", message);
-            Log.i(TAG, "Its Starting to process");
-            DocumentEmotion emotion = service.getEmotion(myMap).execute();
-            DocumentEmotion.Emotion emo = emotion.getEmotion();
-            Log.i(TAG, "Its got processed");
+                Map<String, Object> myMap = new HashMap<String, Object>();
+                myMap.put("text", new String(message));
+                Log.i(TAG, "Its Starting to process : " + message);
+//                service.getEmotion(myMap).enqueue(new ServiceCallback<DocumentEmotion>() {
+//                    @Override
+//                    public void onResponse(DocumentEmotion emotion) {
+//                        Log.i(TAG, "Its got processed 0");
+//                        DocumentEmotion.Emotion emo = emotion.getEmotion();
+//                        Log.i(TAG, "Its got processed");
+//
+//                        Log.i(TAG, "Anger : " + emo.getAnger());
+//                        Log.i(TAG, "Disgust : " + emo.getDisgust());
+//                        Log.i(TAG, "Fear : " + emo.getFear());
+//                        Log.i(TAG, "Joy : " + emo.getJoy());
+//                        Log.i(TAG, "Sadness : " + emo.getSadness());
+//
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Exception e) {
+//                        Log.i(TAG, "Error is : "+e.getMessage());
+//                    }
+//                });
+                ServiceCall<DocumentEmotion> call = service.getEmotion(myMap);
+                if(call !=null)
+                    Log.i(TAG, "Its got processed 1");
+                DocumentEmotion emotion = call.execute();
+                Log.i(TAG, "Its got processed 0");
+                DocumentEmotion.Emotion emo = emotion.getEmotion();
+                Log.i(TAG, "Its got processed");
 
-            Log.i(TAG, "Anger : "+emo.getAnger());
-            Log.i(TAG, "Disgust : "+emo.getDisgust());
-            Log.i(TAG, "Fear : "+emo.getFear());
-            Log.i(TAG, "Joy : "+emo.getJoy());
-            Log.i(TAG, "Sadness : "+emo.getSadness());
-
+                Log.i(TAG, "Anger : " + emo.getAnger());
+                Log.i(TAG, "Disgust : " + emo.getDisgust());
+                Log.i(TAG, "Fear : " + emo.getFear());
+                Log.i(TAG, "Joy : " + emo.getJoy());
+                Log.i(TAG, "Sadness : " + emo.getSadness());
+            }catch (Exception ex){
+                Log.i(TAG, ex.getMessage());
+            }
             return null;
         }
     }
